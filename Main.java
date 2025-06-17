2ca2ed5e9c553a6065d7b5ad6740e537f514c57a
hitohito-ari <ariyuu782@gmail.com>
2025-06-17 10:50:52 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("名前を入力してください");
+        System.out.println("予定を入力してください");
         String name = scanner.nextLine();
 
         System.out.println("日付");
         String date = scanner.nextLine();
 
-        System.out.println("予定の時間");
+        System.out.println("時間");
         String shift = scanner.nextLine();
 
         System.out.println("スケジュール");
